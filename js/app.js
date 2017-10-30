7cca12e4659a5f967c6288484fb87d7061448386
sarah lund <sarah.christine.lund@gmail.com>
2017-10-30 12:10:02 -0400

    check

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@
 // Let's get a reference to all our DOM elements
 // and store them in an object
 // @hint: use document.querySelector()
-//var total = 0;
+
 const els = {
       total:      null,
       entries:    null,
@@ -18,7 +18,7 @@ els.entryForm = document.querySelector('#entry');
 els.inputField = els.entryForm.querySelector('input');
 els.entries =[];
 
-
+//query each index within the object
 // helper functions
 
 /**
@@ -32,27 +32,29 @@ const addRow = function (content) {
   // Get a reference to the table
   var tableRef = document.getElementById("entries");
   // Insert a row in the table at row index 0
-  var newRow = tableRef.insertRow(0);
+  var newRow = tableRef.insertRow(-1);
   // Insert a cell in the row at index 0
   newRow.insertCell(0);
   var newCell = newRow.insertCell(1);
   // injects content into cell
   newCell.innerHTML = content;
-
+//return row
 };
-
+//applies to every object that can contain html
 /**
  * @func removeDecimal
  * @desc parses a string and converts it to a whole number (decimal removed)
  * @param {String} str
  * @returns {Number} the incoming number multiplied by 100
+ returns a string representation of numObj that does not use exponential notation and has specified length after decimal
  */
 const removeDecimal = function (str) {
   var float = parseFloat(str);
   return (float * 100).toFixed();
 
 };
-
+//or instead of toFixed, return Math.trunc(float * 100)
+//to fixed returns a string representation of numObj that does not use exponential notation and has in this instance (2) digits after the decimal place
 /**
  * @func dollarFormat
  * @desc takes an integer and formats a string in dollar format
@@ -75,7 +77,15 @@ const dollarFormat = function (num) {
 const handleFormSubmit = function (event) {
   // stop the event from happening
   event.preventDefault();
-
+//create function called validate to produce alert if input is NaN
+//const val= els.inputField.value
+//use conditional logic to create alert if NaN occurs
+//if(true) {
+//alert()
+//els.entryForm.reset()
+//return
+//}
+}
   // note: remember that form fields ALWAYS contain text
   var strInput = els.inputField.value;
   var newValue = removeDecimal(strInput);
@@ -94,11 +104,13 @@ const handleFormSubmit = function (event) {
 
   els.total = parseFloat(els.total) + parseFloat(newValue);
   document.querySelector("#total").innerHTML = dollarFormat(els.total);
-
+  //using parse float to stop total from being produced as a string
   // reset the form to clear out anything previously typed
 
   els.inputField.value = "";
 };
+// or you can els.entryForm.reset(); which will put back placeholder, "How much?"
+
 
 // event handlers
 /**
